546dda98b35abb0b7b9450f1c8e5aaf839f27314
Carlos Vicente <carlosvicente200@gmail.com>
2016-03-06 22:44:07 +0000

    Implemented AggregateHydrator

---
+++ Playground.Domain/Model/AggregateHydrator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Playground.Domain.Events;
+
+namespace Playground.Domain.Model
+{
+    public class AggregateHydrator : IAggregateHydrator
+    {
+        public TAggregateRoot HydrateAggregateWithEvents<TAggregateRoot>(
+            TAggregateRoot aggregateRootBase, 
+            ICollection<IEvent> domainEvents) 
+            where TAggregateRoot : AggregateRoot
+        {
+            foreach (var domainEvent in domainEvents)
+            {
+                Do(aggregateRootBase, domainEvent);
+            }
+
+            return aggregateRootBase;
+        }
+
+        private void Do<TAggregateRoot, TDomainEvent>(
+            TAggregateRoot aggregateRootBase, TDomainEvent domainEvent)
+            where TAggregateRoot : AggregateRoot
+            where TDomainEvent : IEvent
+        {
+            ((IEmit<TDomainEvent>)aggregateRootBase).Apply(domainEvent);
+        }
+    }
+}
