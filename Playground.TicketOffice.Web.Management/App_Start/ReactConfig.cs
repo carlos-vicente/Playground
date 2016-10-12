fb65b00b646e9c3009bc901db863a135d42b4293
Carlos Vicente <carlos.vicente@sky.uk>
2016-10-12 16:29:19 +0100

    Updated packages

--- Playground.TicketOffice.Web.Management/App_Start/ReactConfig.cs
+++ Playground.TicketOffice.Web.Management/App_Start/ReactConfig.cs
@@ -1,44 +1,28 @@
-using System.IO;
-using System.Linq;
-using System.Web;
-using Playground.TicketOffice.Web.Management;
 using React;
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(ReactConfig), "Configure")]
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Playground.TicketOffice.Web.Management.ReactConfig), "Configure")]
 
 namespace Playground.TicketOffice.Web.Management
 {
-    public static class ReactConfig
-    {
-        public static void Configure()
-        {
-            // If you want to use server-side rendering of React components, 
-            // add all the necessary JavaScript files here. This includes 
-            // your components as well as all of their dependencies.
-            // See http://reactjs.net/ for more information. Example:
-
-            string path = $"{HttpRuntime.AppDomainAppPath}Scripts\\Components";
-
-            var entries = Directory
-                .GetFileSystemEntries(path, "*.jsx", SearchOption.AllDirectories);
-
-            var configuration = ReactSiteConfiguration.Configuration;
-
-            var virtualPaths = entries
-                .Select(e => e.Replace(HttpRuntime.AppDomainAppPath, "~\\"));
-
-            foreach (var component in virtualPaths)
-            {
-                configuration.AddScript(component);
-            }
-
-            // If you use an external build too (for example, Babel, Webpack,
-            // Browserify or Gulp), you can improve performance by disabling 
-            // ReactJS.NET's version of Babel and loading the pre-transpiled 
-            // scripts. Example:
-            //ReactSiteConfiguration.Configuration
-            //	.SetLoadBabel(false)
-            //	.AddScriptWithoutTransform("~/Scripts/bundle.server.js")
-        }
-    }
+	public static class ReactConfig
+	{
+		public static void Configure()
+		{
+			// If you want to use server-side rendering of React components, 
+			// add all the necessary JavaScript files here. This includes 
+			// your components as well as all of their dependencies.
+			// See http://reactjs.net/ for more information. Example:
+			//ReactSiteConfiguration.Configuration
+			//	.AddScript("~/Scripts/First.jsx")
+			//	.AddScript("~/Scripts/Second.jsx");
+			
+			// If you use an external build too (for example, Babel, Webpack,
+			// Browserify or Gulp), you can improve performance by disabling 
+			// ReactJS.NET's version of Babel and loading the pre-transpiled 
+			// scripts. Example:
+			//ReactSiteConfiguration.Configuration
+			//	.SetLoadBabel(false)
+			//	.AddScriptWithoutTransform("~/Scripts/bundle.server.js")
+		}
+	}
 }
